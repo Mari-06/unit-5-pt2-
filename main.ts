8f011d5737ef32bab0db3c06811d577886b08f8d
Mr-Harris <124732072+Mr-Harris@users.noreply.github.com>
2023-02-08 14:29:56 -0600

    Password change

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
 scene.setBackgroundColor(3)
+let password = game.askForString("WHAT IS THE PASSWORD?", 24)
+if (password == "Pizzas" || password == "Never Gonna Give You Up!") {
+    game.splash("LOGIN SUCCESSFUL")
+} else {
+    game.splash("LOGIN FAILURE")
+}
